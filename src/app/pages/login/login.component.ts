b2438ad1f6eccbe6d5facbd872458fdad0881c85
Lucia <lucia.jmunoz@alumnos.upm.es>
2023-05-07 18:59:09 +0200

    añaidido login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  email : String | undefined ;
+  password : String | undefined;
+  
+  constructor() { }
+
+
+  login(){
+    console.log("logeado");
+  }
+
+}
