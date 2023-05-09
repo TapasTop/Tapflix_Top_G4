40b6ad9fa8f6f2c0296bea3cf30c22d7703fda66
Lucia <lucia.jmunoz@alumnos.upm.es>
2023-05-09 18:23:26 +0200

    implementado interceptor

--- src/app/interceptor/interceptor.service.ts
+++ src/app/interceptor/interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,28 @@ export class InterceptorService implements HttpInterceptor {
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+    const token: string  | null = localStorage.getItem('token');
+
+    let request = req;
+
+    if (token) {
+      request = req.clone({
+        setHeaders: {
+          authorization: `Bearer ${ token }`
+        }
+      });
+    }
+    
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+
+        if (err.status === 401) {
+          console.log("error");
+        }
+
+        return throwError( err );
+
+      })
+    );
   }
 }
